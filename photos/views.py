ea01aa90a7363fe66cf4a6f3fc24a523e1a20db5
Akhadafi <khadafiar495@gmail.com>
2022-06-08 11:54:41 +0700

    Finish

--- photos/views.py
+++ photos/views.py
@@ -5,8 +5,13 @@ from .models import Category, Photo
 
 # Create your views here.
 def gallery(request):
+    category = request.GET.get("category")
+    if category == None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name__contains=category)
+
     categories = Category.objects.all()
-    photos = Photo.objects.all()
 
     context = {
         "categories": categories,
